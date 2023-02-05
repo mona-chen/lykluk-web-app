42274a50aa9081a46b10c02722313ba9936dd7c3
NAJIB-B <najibajibasu@gmail.com>
2023-02-05 11:38:13 +0100

    added profile page initials

--- components/profile/Profile.jsx
+++ components/profile/Profile.jsx
@@ -1,109 +1,71 @@
-import React, { useEffect, useState } from 'react'
-import style from './style.module.css'
-import env from '../../env';
-import { useSelector } from 'react-redux';
-import Image from 'next/image';
-import { ButtonPrimary } from '../buttons/ButtonReuse';
-import { profileIcons } from '../../assets/icons/profileIcons';
-
-export default function ProfileComponent() {
-
-    const [account,setAccount] = useState({})
-    const {profile } = account;
-
-    // console.warn(user)
-    const { user, loadLogout } = useSelector((state) => state.user)
-
-    /* we are temporarily storing redux states in react state to prevent hydration error.
-    this is issue #1 and should be resolved */
-    useEffect(() => {
-      setAccount(user)
-    }, [user])
-
-    //hydration error fix ends here
-    
-
-  return (
-    <div className={style.profile_wrapper}>
-        <div className={style.profile_details_wrapper}>
-            <figure>
-            <Image src={env.cloudfront + profile?.avatar} alt={'profile'} width='500' height='500' />
-            </figure>
-            <div className={style.top_details_wrapper}>
-            <div className={style.top_details}>
-                    <div>
-                        <h2>
-                            {profile?.name}
-                        </h2>
-                        <span>
-                            {account?.username}
-                            <figure>{profileIcons.share}</figure>  
-                        </span>
-                    </div>
-                    <div>
-                        <ButtonPrimary
-                        fill='white'
-                        icon={profileIcons.edit}
-                        svg
-                        padding="1rem 1rem"
-                        gap= "2rem"
-                        border='1px solid #8E00FE'
-                        color="black"
-                        >
-                            Edit Profile
-
-                        </ButtonPrimary>
-
-                    </div>
-
+import style from "./style.module.css"
+import Image from "next/image";
+const Profile = () => {
+    return ( 
+        <div className={style.main_profile_wrapper}>
+          <div className={style.top_section}>
+            <div className={style.first_div}>
+                <p>Jacob west</p>
+                <Image src="/edit_profile.svg" height={20} width={20}></Image>
             </div>
-            <div className={style.bottom_details}>
-                    <div>
-                        <div>
-                            <b>134</b>
-                            Following
-                        </div>
-
-                        <div>
-                            <b>134</b>
-                            Followers
-                        </div>
-
-                        <div>
-                            <b>134</b>
-                            Likes
-                        </div>
-                   
-                    </div>
-                    <div>
-                        <figure>
-                            {profileIcons.add_account}
-                        </figure>
-
-                        <figure>
-                            {profileIcons.gear}
-                        </figure>
-                    </div>
-
+            <div className={style.profile_pic_div}>
+            <Image src="/profile_pic.svg" height={90} width={100}></Image>  
             </div>
+            <p className={style.profile_username}>
+                @najib2557
+            </p>
+            <div className={style.bio_div}>
+                <h4 className={style.header}>
+                    Bio:
+                </h4>
+                <p>
+                I am Video Editor and i am doing 
+                this for 3 years long thanks for 
+                viewing my  profile.
+                </p>
             </div>
-
-            
-        </div>
-
-        <div className={style.bio_contain}>
-                <h2>Bio</h2>
-                <small>A tiger ran over a lion</small>
-                
+            <div className={style.profile_stats_div}>
+                <div className={style.section}>
+                    <p className={style.number}>2.9k</p>
+                    <p className={style.text}>following</p>
+                </div>
+                <div className={style.section}>
+                    <p className={style.number}>26.9k</p>
+                    <p className={style.text}>followers</p>
+                </div>
+                <div className={style.section}>
+                    <p className={style.number}>1.4k</p>
+                    <p className={style.text}>post</p>
+                </div>
+               
             </div>
-
-            <div className={style.video_gallery}>
-                <div className={style.gallery_toggle}>
-
+          </div>
+          <div className={style.post_switch_div}>
+            <div className={style.posts}>
+                <div  className={style.icon}>
+                    <Image src="/tabs_Icon.svg" width={25} height={23}/>
                 </div>
             </div>
-             
-        
-    </div>
-  )
+    
+            <div className={style.hidden}>
+                <div  className={style.icon}>
+                    <Image src="/heart_hide.svg" width={25} height={23}/>
+                </div>
+            </div>
+          </div>
+          <div className={style.posts_div}>
+            <div className={style.video}>
+                <Image src="/video_post.png" height={270} width={190}/>
+            </div>
+            <div className={style.video}>
+                <Image src="/video_post.png" height={270} width={190}/>
+            </div>
+            <div className={style.video}>
+                <Image src="/video_post.png" height={270} width={190}/>
+            </div>
+          </div>
+        </div>
+     );
 }
+ 
+export default Profile;
