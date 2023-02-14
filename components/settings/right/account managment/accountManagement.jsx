0b6824d28048ac776fa5b684130e51afcecf0371
NAJIB-B <najibajibasu@gmail.com>
2023-02-14 20:23:10 +0100

    starting styling of settings

--- components/settings/right/account managment/accountManagement.jsx
+++ components/settings/right/account managment/accountManagement.jsx
@@ -1,16 +1,51 @@
-import InputElement from '../../../InputElement/InputElement'
-import PasswordInputField from './passwordInputField/passwordInputField'
-import { passwordInputType } from './passwordInputField/passwordInputFieldWithoutLabel'
+/* eslint-disable import/no-unresolved */
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
+import flags from 'react-phone-number-input/flags'
+
+import { useState } from 'react'
+import styles from './style.module.css'
+
+const defaultFormFields = {
+  email: '',
+  phoneNumber: '',
+  password: '',
+}
 const AccountManagement = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields)
+  const { email, phoneNumber, password } = formFields
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target
+    setFormFields({ ...formFields, [name]: value })
+  }
   return (
     <div>
       <p>Email</p>
-      <InputElement
-        type="email"
-        placeholder="Change your email address"
-      ></InputElement>
+      <input
+        name="email"
+        onChange={changeHandler}
+        type="text"
+        placeholder="change your email"
+        className={styles.input_elements}
+      ></input>
+
+      <p>Phone Number</p>
+      <PhoneInput
+        flags={flags}
+        onChange={changeHandler}
+        defaultCountry="NG"
+        international={true}
+        value={phoneNumber}
+      />
       <p>Password</p>
-      <input type="password" placeholder="***********" />
+      <input
+        name="password"
+        type="password"
+        onChange={changeHandler}
+        placeholder="***********"
+        className={styles.input_elements}
+      />
     </div>
   )
 }
